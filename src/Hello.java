7f7c5010c3fada124a3860e160e4e41a14618711
adrienchevrier <adrien.chevrier.pro@gmail.com>
2016-05-03 11:05:28 +0900

    The git project is finally created

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by adrien on 03/05/16.
+ * EducationGame project class
+ */
+public class Hello {
+    public void SayHello(){
+        System.out.println("Hello everybody!");
+    }
+}
