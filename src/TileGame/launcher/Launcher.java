8ea2751231c757c15ead048cdb2c90306b735434
adrienchevrier <adrien.chevrier.pro@gmail.com>
2016-05-16 18:33:29 +0900

    Soldiers moves on a map

--- src/TileGame/launcher/Launcher.java
+++ src/TileGame/launcher/Launcher.java
@@ -10,7 +10,7 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        Game game = new Game("Game",500,300);
+        Game game = new Game("Game",1000,1000);
         game.start();
     }
 }
