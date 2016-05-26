4119bc565221358ab2bd5d7b72f0046616fcb239
reinaldomaslim <rmaslim001@e.ntu.edu.sg>
2016-05-26 22:37:26 +0900

    melee done

---
+++ src/entities/EntityManager.java
@@ -0,0 +1,87 @@
+package entities;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import entities.armies.Player;
+import handler.Handler;
+
+public class EntityManager {
+	
+	private Handler handler;
+	private Player player;
+	private ArrayList<Entity> entities;
+	private Entity e;
+	private Comparator<Entity> renderSorter = new Comparator<Entity>(){
+		@Override
+		public int compare(Entity a, Entity b) {
+			if(a.getY() + a.getHeight() < b.getY() + b.getHeight())
+				return -1;
+			return 1;
+		}
+	};
+	
+	
+	public EntityManager(Handler handler, Player player){
+		this.handler=handler;
+		this.player=player;
+		entities=new ArrayList<Entity>();
+		this.addEntity(player);
+	}
+	
+	public void tick(){
+		for(int i = 0;i < entities.size();i++){
+			e = entities.get(i);
+			e.tick();
+			
+			if(!e.isAlive())
+				entities.remove(e);
+			
+		}
+		entities.sort(renderSorter);
+	}
+	
+	public void render(Graphics g){
+		for(Entity e:entities){
+			e.render(g);
+		}
+		
+	}
+	
+	public void addEntity(Entity e){
+		entities.add(e);
+	}
+	
+	
+	//GETTERS and SETTERS
+	
+	public Handler getHandler() {
+		return handler;
+	}
+
+	public void setHandler(Handler handler) {
+		this.handler = handler;
+	}
+	
+	
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public ArrayList<Entity> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(ArrayList<Entity> entities) {
+		this.entities = entities;
+	}
+
+
+	
+	
+}
---
+++ src/inputs/MouseManager.java
@@ -0,0 +1,93 @@
+package inputs;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MouseManager implements MouseListener, MouseMotionListener{
+	
+	private boolean leftPressed, rightPressed;
+	private int mouseX, mouseY;
+	
+	
+	public MouseManager(){
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		mouseX=e.getX();
+		mouseY=e.getY();
+		
+		
+	}
+
+	//GETTERS
+	
+	public boolean isLeftPressed() {
+		return leftPressed;
+	}
+
+	public boolean isRightPressed() {
+		return rightPressed;
+	}
+
+	public int getMouseX() {
+		return mouseX;
+	}
+
+	public int getMouseY() {
+		return mouseY;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getButton()==MouseEvent.BUTTON1){
+			leftPressed=true;
+		}
+		else if(e.getButton()==MouseEvent.BUTTON3){
+			rightPressed=true;
+		}
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getButton()==MouseEvent.BUTTON1){
+			leftPressed=false;
+		}
+		else if(e.getButton()==MouseEvent.BUTTON3){
+			rightPressed=false;
+		}
+	}
+	
+	
+
+}
