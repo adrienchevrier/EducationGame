c7fdcd08bd396e23296f42fbf925b0f5120344ec
Bae Hyunmo <hyunmo27@gmail.com>
2016-06-05 12:07:13 +0900

    Change GameB map tile variable type int to Tile

--- src/tiles/Goal.java
+++ src/tiles/Goal.java
@@ -4,8 +4,14 @@ import java.awt.image.BufferedImage;
 
 import gfx.Assets;
 
-public class Goal extends Tile {
+public class Goal extends Tile implements Swapable {
     public Goal(int id) {
         super(Assets.goal, id);
     }
+
+	@Override
+	public void swap(Tile src, int direction) {
+		// TODO Auto-generated method stub
+		
+	}
 }
